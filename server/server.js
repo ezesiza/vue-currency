553a8a0ce1c6ed48db7b6b8fc8009ed95a5e0d8e
ezesiza <ezesiza@gmail.com>
2020-08-21 10:09:07 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -11,19 +11,10 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// database configuration
-// const db = require("./utils/keys").mongoURI;
-
-// Test Route
 app.get('/', (req, res) => res.send('Eze here! Codes works'))
 // routes
 
 app.use('/app', coinbase)
-
-// if (config.util.getEnv('NODE_ENV') !== 'test') {
-//     //use morgan to log at command line
-//     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-// }
 
 const port = process.env.PORT || 5006
 
